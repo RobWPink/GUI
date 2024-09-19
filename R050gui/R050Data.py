6454c86fc578d5e7c391d8a3d120674c3e0268c4
RobWPink <robert.pink@oneh2.com>
2024-09-19 14:48:59 -0400

    added args and tcp close

--- R050gui/R050Data.py
+++ R050gui/R050Data.py
@@ -1,8 +1,18 @@
 from pyModbusTCP.client import ModbusClient
 from pyModbusTCP.utils import test_bit
-import sys, time, ctypes,subprocess, socket
+import sys, time, ctypes,subprocess, socket, optparse
 
-htop = subprocess.Popen(["/usr/bin/python3", "/home/pi/R050gui/R050GUI.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+parser = optparse.OptionParser()
+
+parser.add_option('-I','--ip',action="store",type="string",dest='ip',help ='Set device IP (Default: "127.0.0.1")',default='127.0.0.1')
+parser.add_option('-p','--port',action="store",type="string",dest='port',help ='Set device port (Default: 520)',default='520')
+parser.add_option('-d','--id',action="store",type="string",dest='idList',help ='Specify list of Compressor ID\'s (Default:"1,2,3,4")',default='1,2,3,4')
+parser.add_option('-v','--verbose',action="store_true",dest='verb',help ='Print out all values')
+parser.add_option('-g','--gui',action="store_true",dest='gui',help ='Choose NOT to start GUI')
+
+(options, args) = parser.parse_args()
+if not options.gui:
+	htop = subprocess.Popen(["/usr/bin/python3", "/home/pi/R050gui/R050GUI.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 ReformerIDs = [1,2,3,4]
 portNum = 21792
@@ -68,9 +78,7 @@ def main():
 		for i in ReformerIDs:
 			#mb.append(ModbusClient(host="127.0.0.1", port=502, unit_id=i, timeout=1,auto_open=True))
 			mb.append(ModbusClient(host="uk1.pitunnel.com", port=portNum, unit_id=i, timeout=5,auto_open=True))
-		
-		seconds = [ctypes.c_uint32(0).value]*len(ReformerIDs)
-		prevSeconds = [0]*len(ReformerIDs)
+
 		activeReformers = [True]*len(ReformerIDs)
 		warning1 = ctypes.c_uint32(0).value
 		warning2 = ctypes.c_uint32(0).value
@@ -80,7 +88,7 @@ def main():
 		allErrors = [0]*len(ReformerIDs)
 		allWarnings = [0]*len(ReformerIDs)
 		tableData = {}
-		tmr = time.perf_counter()
+
 		cnt = 0
 		errorMsg = ""
 		socketError = True
@@ -99,6 +107,8 @@ def main():
 					if not len(data) >= 100:
 						errorMsg = "0"
 					errorMsg = ""
+					if options.verb:
+						print(data)
 					activeReformers[cnt] = True
 					socketError = False
 			except socket.gaierror:
@@ -115,8 +125,9 @@ def main():
 						lst = lst[:-1]
 					errorMsg = "1Reformer #(s)"+lst+" connection timed out"
 			except KeyboardInterrupt:
-				htop.terminate()
-				for i in len(mb):
+				if not options.gui:
+					htop.terminate()
+				for i in range(len(mb)):
 					mb[i].close()
 				sys.exit()
 			if all(v == 0 for v in activeReformers):
@@ -194,24 +205,27 @@ def main():
 			with open('data.txt', 'a') as fd:
 				for a in tableData:
 					fd.write(tableData[a])
-			if htop.poll() != None:
-				print("GUI quit")
-				for i in len(mb):
-					mb[i].close()
-				sys.exit(1)
+			if not options.gui: 
+				if htop.poll() != None:
+					print("GUI quit")
+					for i in range(len(mb)):
+						mb[i].close()
+					sys.exit(1)
 			if cnt >= (len(ReformerIDs)-1):
 				cnt = 0
 			else:
 				cnt = cnt + 1
 			continue
 	except KeyboardInterrupt:
-		htop.terminate()
-		for i in len(mb):
+		if not options.gui:
+			htop.terminate()
+		for i in range(len(mb)):
 			mb[i].close()
 		sys.exit()
 	except Exception as e:
-		htop.terminate()
-		for i in len(mb):
+		if not options.gui:
+			htop.terminate()
+		for i in range(len(mb)):
 			mb[i].close()
 		sys.exit()
 
